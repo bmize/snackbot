bd576699d544d077748868d22579ded99c07c10b
brenden <brenden@indi.com>
2023-02-10 15:05:45 -0800

    Added initial bot code with a dice roll command

---
+++ discord/commands/rng.js
@@ -0,0 +1,41 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+/**
+ * @param {number} sides The number of sides on the die
+ * @returns {number}
+ */
+function rollDie(sides) {
+  if (Number.isInteger(sides)) {
+    return Math.floor(Math.random() * sides) + 1;
+  }
+}
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('roll')
+    .setDescription('Rolls dice in standard D&D style (e.g. "2d6")')
+    .addIntegerOption((option) =>
+      option
+        .setName('count')
+        .setDescription('The number of dice you want to roll.')
+        .setRequired(true)
+    )
+    .addIntegerOption((option) =>
+      option
+        .setName('sides')
+        .setDescription('The number of sides on each die being rolled.')
+        .setRequired(true)
+    ),
+  async execute(interaction) {
+    const count = interaction.options.getInteger('count');
+    const sides = interaction.options.getInteger('sides');
+    if (count && sides) {
+      const rolls = Array.from({ length: count }, (value, index) => rollDie(sides));
+      return interaction.reply(
+        `🎲 Rolling **${count}d${sides}** 🎲\nYou rolled... **${rolls.join(', ')}**`
+      );
+    } else {
+      return interaction.reply('Required values were missing.');
+    }
+  },
+};
